f28ef1498be77129c465cf01dce1f8a4d463f9cd
john <jayelwhy@yahoo.com>
2015-06-12 11:16:28 -0700

    include equal method for artwork class

--- lib/artwork.rb
+++ lib/artwork.rb
@@ -1,35 +1,28 @@
 class Artwork
-  attr_reader(:description, :id)
+  attr_reader(:description, :museum_id)
 
   define_method(:initialize) do |attributes|
     @description = attributes.fetch(:description)
-    @id = attributes.fetch(:id)
+    @museum_id = attributes.fetch(:museum_id)
   end
+
   define_singleton_method(:all) do
     returned_artworks = DB.exec('SELECT * FROM artworks;')
     artworks = []
     returned_artworks.each() do |artwork|
       description = artwork.fetch('description')
-      id = artwork.fetch('id').to_i()
-      artworks.push(Artwork.new({:description => description, :id => id}))
+      museum_id = artwork.fetch('museum_id').to_i()
+      artworks.push(Artwork.new({:description => description, :museum_id => museum_id}))
     end
     artworks
   end
+
   define_method(:save) do
-    result = DB.exec("INSERT INTO artworks (description) VALUES ('#{@description}') RETURNING id;")
-    @id = result.first().fetch('id').to_i()
-  end
-  define_method(:==) do |another_artwork|
-    self.description().==(another_artwork.description()).&(self.id().==(another_artwork.id()))
+    DB.exec("INSERT INTO artworks (description, museum_id) VALUES ('#{@description}', #{@museum_id});")
   end
 
-  define_singleton_method(:find) do |id|
-    found_artwork = nil
-    Artwork.all().each() do |artwork|
-      if artwork.id().==(id)
-        found_artwork = artwork
-      end
-    end
-    found_artwork
+  define_method(:==) do |another_artwork|
+    self.description().==(another_artwork.description()).&(self.museum_id().==(another_artwork.museum_id()))
   end
+
 end
--- spec/artwork_spec.rb
+++ spec/artwork_spec.rb
@@ -3,14 +3,14 @@ require("spec_helper")
 describe(Artwork) do
   describe(".all") do
     it("empty at first and until an object is saved") do
-      artwork2 = Artwork.new({:description => 'jar', :id => nil})
+      artwork2 = Artwork.new({:description => 'jar', :museum_id => nil})
       expect(Artwork.all()).to(eq([]))
     end
   end
 
   describe("#save") do
     it("is will show a list of all of the artworks") do
-      artwork = Artwork.new({:id => 1, :description => 'blank paper'})
+      artwork = Artwork.new({:description => 'blank paper', :museum_id => 1})
       artwork.save
       expect(Artwork.all()).to(eq([artwork]))
     end
@@ -18,19 +18,10 @@ describe(Artwork) do
 
   describe('#==') do
     it('is the same list if it has the same name') do
-      artwork1 = Artwork.new({:description => 'Old House', :id => nil})
-      artwork2 = Artwork.new({:description => 'Old House', :id => nil})
+      artwork1 = Artwork.new({:description => 'Old House', :museum_id => nil})
+      artwork2 = Artwork.new({:description => 'Old House', :museum_id => nil})
       expect(artwork1).to(eq(artwork2))
     end
   end
 
-  describe('.find') do
-  it('returns a museu by its id') do
-    test_artwork = Artwork.new({:description => 'Old House', :id => nil})
-    test_artwork.save()
-    test_artwork2 = Artwork.new({:description => 'Cool Place', :id => nil})
-    test_artwork2.save()
-    expect(Artwork.find(test_artwork.id())).to(eq(test_artwork))
-  end
-end
 end
