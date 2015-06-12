d6b6c6d40691b2ad9f0bdec8acb8c56979fd9fb4
john <jayelwhy@yahoo.com>
2015-06-12 12:40:41 -0700

    include an index landing page

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/museum')
+require('pry')
 
 also_reload('lib/**/*.rb')
 require('pg')
@@ -9,5 +10,14 @@ DB = PG.connect({:dbname => "museum_tracker"})
 
 
 get('/') do
+  @museums = Museum.all
   erb(:index)
+end
+
+post('/') do
+  name = params.fetch("name")
+  new_museum = Museum.new({:name => name, :id => nil})
+  new_museum.save
+  @museums = Museum.all
+  redirect back
 end
