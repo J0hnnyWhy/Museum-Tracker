8c84615e4f0435418b59f8dc48a5afcd449a0375
john <jayelwhy@yahoo.com>
2015-06-12 10:45:22 -0700

    include find and equal methods for artwork class

--- spec/artwork_spec.rb
+++ spec/artwork_spec.rb
@@ -15,4 +15,22 @@ describe(Artwork) do
       expect(Artwork.all()).to(eq([artwork]))
     end
   end
+
+  describe('#==') do
+    it('is the same list if it has the same name') do
+      artwork1 = Artwork.new({:description => 'Old House', :id => nil})
+      artwork2 = Artwork.new({:description => 'Old House', :id => nil})
+      expect(artwork1).to(eq(artwork2))
+    end
+  end
+
+  describe('.find') do
+  it('returns a museu by its id') do
+    test_artwork = Artwork.new({:description => 'Old House', :id => nil})
+    test_artwork.save()
+    test_artwork2 = Artwork.new({:description => 'Cool Place', :id => nil})
+    test_artwork2.save()
+    expect(Artwork.find(test_artwork.id())).to(eq(test_artwork))
+  end
+end
 end
