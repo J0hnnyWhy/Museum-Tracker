207c7b587b9f70a2b8ead6fe33f030885783a791
john <jayelwhy@yahoo.com>
2015-06-12 10:32:13 -0700

    include equal and save methods for museum class

--- lib/museum.rb
+++ lib/museum.rb
@@ -3,24 +3,33 @@ class Museum
 
   define_method(:initialize) do |attributes|
     @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
   end
-
   define_singleton_method(:all) do
-    returned_museums = DB.exec("SELECT * FROM museums;")
+    returned_museums = DB.exec('SELECT * FROM museums;')
     museums = []
     returned_museums.each() do |museum|
-      name = museum.fetch("name")
-      museums.push(Museum.new({:name => name}))
+      name = museum.fetch('name')
+      id = museum.fetch('id').to_i()
+      museums.push(Museum.new({:name => name, :id => id}))
     end
     museums
   end
-
   define_method(:save) do
     result = DB.exec("INSERT INTO museums (name) VALUES ('#{@name}') RETURNING id;")
     @id = result.first().fetch('id').to_i()
   end
+  define_method(:==) do |another_museum|
+    self.name().==(another_museum.name()).&(self.id().==(another_museum.id()))
+  end
 
-  define_method(:==) do |another_list|
-    self.name().==(another_list.name()).&(self.id().==(another_list.id()))
+  define_singleton_method(:find) do |id|
+    found_museum = nil
+    Museum.all().each() do |museum|
+      if museum.id().==(id)
+        found_museum = museum
+      end
+    end
+    found_museum
   end
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -2,21 +2,37 @@ require("spec_helper")
 
 describe(Museum) do
   describe(".all") do
-    it("is will show a list of all of the museums") do
-      museum1 = Museum.new({:name => 'the place', :id => nil})
-      museum1.save
+    it("empty at first and until an object is saved") do
       museum2 = Museum.new({:name => 'new place', :id => nil})
-      museum2.save
-      expect(Museum.all()).to(eq(['museum1', 'museum2']))
+      expect(Museum.all()).to(eq([]))
     end
   end
 
   describe("#save") do
     it("is will show a list of all of the museums") do
-      museum1 = Museum.new({:name => 'the place'})
-      museum1.save
-      expect(Museum.all()).to(eq([museum1]))
+      museum = Museum.new({:id => 1, :name => 'the place'})
+      museum.save
+      expect(Museum.all()).to(eq([museum]))
     end
   end
+
+  describe('#==') do
+    it('is the same list if it has the same name') do
+      museum1 = Museum.new({:name => 'Old House', :id => nil})
+      museum2 = Museum.new({:name => 'Old House', :id => nil})
+      expect(museum1).to(eq(museum2))
+    end
+  end
+
+  describe('.find') do
+  it('returns a museu by its id') do
+    test_museum = Museum.new({:name => 'Old House', :id => nil})
+    test_museum.save()
+    test_museum2 = Museum.new({:name => 'Cool Place', :id => nil})
+    test_museum2.save()
+    expect(Museum.find(test_museum.id())).to(eq(test_museum))
+  end
+end
+
 
 end
